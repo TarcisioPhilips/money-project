7a1e5d3be53bd13155847ea11ed649b17082a97e
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-06-14 15:31:40 -0300

    feat: Radio Group Button component

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -22,13 +22,14 @@ export function NewTransactionModal() {
             <input type="text" placeholder="Descrição" required />
             <input type="number" placeholder="Preço" required />
             <input type="text" placeholder="Categoria" required />
+
             <TransactionType>
-              <TransactionTypeButton variant="income">
+              <TransactionTypeButton variant="income" value="income">
                 <ArrowCircleUp size={24} />
                 Entrada
               </TransactionTypeButton>
 
-              <TransactionTypeButton variant="outcome">
+              <TransactionTypeButton variant="outcome" value="outcome">
                 <ArrowCircleDown size={24} />
                 Saida
               </TransactionTypeButton>
