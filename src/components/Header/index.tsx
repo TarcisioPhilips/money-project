9f6ac290e14fe9cafc98566c0c783c05d6bc286e
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-06-14 12:16:25 -0300

    feat: creating the modal structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,27 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import logoImg from '../../assets/meu-logo.svg'
-
+import logoImg from "../../assets/meu-logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
-        <img src={logoImg}  alt=""/>
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <img src={logoImg} alt="" />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay>
+              <Dialog.Content>
+                <Dialog.Title>Nova transação</Dialog.Title>
+                <Dialog.Close> Fechar</Dialog.Close>
+              </Dialog.Content>
+            </Dialog.Overlay>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
-  )
+  );
 }
