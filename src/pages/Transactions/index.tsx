7629e278c94f6b764b31914777c1dc50ae44f1b0
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-06-14 08:45:48 -0300

    feat: Transactions Table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../Summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%"> Desenvolvimento de Site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 17.400,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2024</td>
+            </tr>
+            <tr>
+              <td width="50%"> Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">-R$ 17.400,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
